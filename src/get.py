b2d55d0f56431cd0e23038300024e55e61e54ca4
0uts1de <975side@gmail.com>
2019-08-21 04:35:01 +0900

    add funcs

--- src/get.py
+++ src/get.py
@@ -2,16 +2,59 @@ import config
 import json
 
 
-def timeline(count):
+def user_timeline(count = 0, since_id = 0, max_id = 0):
     twitter = config.twitter
     
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json" #タイムライン取得エンドポイント
 
-    params ={"count" : count}
+    params = {}
+
+    if count != 0:
+        params["count"] = count
+
+    if since_id != 0:
+        params["since_id"] = since_id
+
+    if max_id != 0:
+        params["max_id"] = max_id
+
+    if len(params) == 1:
+        print("Too few arguments.")
+        return -1
     
     res = twitter.get(url, params = params)
 
     if res.status_code != 200:
         print("Get timeline failed. : %s" % res.text)
+        return -1
+        
+    return json.loads(res.text)
+
+
+def mentions_timeline(count = 0, since_id = 0, max_id = 0):
+    twitter = config.twitter
+    
+    url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json"
+
+    params = {}
+
+    if count != 0:
+        params["count"] = count
+
+    if since_id != 0:
+        params["since_id"] = since_id
+
+    if max_id != 0:
+        params["max_id"] = max_id
+
+    if len(params) == 0:
+        print("Too few arguments.")
+        return -1
+    
+    res = twitter.get(url, params = params)
+
+    if res.status_code != 200:
+        print("Get timeline failed. : %s" % res.text)
+        return -1
         
     return json.loads(res.text)
--- src/post.py
+++ src/post.py
@@ -36,6 +36,41 @@ def tweet(text, *img):
         print("Post tweet failed : %s" % res.text)
 
 
+def reply(in_reply_to_status_id, text, *img):
+    if len(img) > 4:
+        print("too many images")
+        return -1
+    
+    if type(text) != str:
+        text = str(text)
+
+    twitter = config.twitter
+
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    url_img = "https://upload.twitter.com/1.1/media/upload.json"
+    media_id = []
+    
+    for i in range(len(img)):
+        files = {"media" : img[i]}
+        res_img = twitter.post(url_img, files = files)
+        if res_img.status_code != 200:
+            print("Failed to upload image : %s" % res_img.text)
+            return -1
+        media_id.append(json.loads(res_img.text)["media_id"])
+    
+    if len(media_id) == 0:
+        params = {"status" : text}
+    else:
+        params = {"status" : text, "media_ids" : ",".join(map(str, media_id))}
+    
+    params["in_reply_to_status_id"] = in_reply_to_status_id
+
+    res = twitter.post(url, params = params)
+    
+    if res.status_code != 200:
+        print("Post tweet failed : %s" % res.text)
+
+
 def icon(image):
     twitter = config.twitter
     
@@ -49,6 +84,7 @@ def icon(image):
 
     if res.status_code != 200:
         print("Post image failed. : %s" % res.text)
+        return -1
 
 
 def name(name):
@@ -65,3 +101,4 @@ def name(name):
     
     if res.status_code != 200:
         print("Post name failed. : %s" % res.text)
+        return -1
